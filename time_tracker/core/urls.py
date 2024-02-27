f5df39e1ac693ca260d4899413b32601433be3b5
Rafael Aslanyan <rafaaslan923@gmail.com>
2024-02-28 01:07:06 +0300

    Add endpoint for canceling activity on task.

--- time_tracker/core/urls.py
+++ time_tracker/core/urls.py
@@ -1,11 +1,16 @@
 from django.urls import path
 
-from core.views import StartTaskActivityView
+from core.views import StartTaskActivityView, StopTaskActivityView
 
 urlpatterns = [
     path(
         "start-activity/",
         StartTaskActivityView.as_view(),
         name="start-activity",
+    ),
+    path(
+        "stop-activity/<int:id>/",
+        StopTaskActivityView.as_view(),
+        name="stop-activity",
     )
 ]
--- time_tracker/core/views.py
+++ time_tracker/core/views.py
@@ -1,9 +1,11 @@
-from django.db import transaction
+import datetime
+
+from django.core.exceptions import ObjectDoesNotExist
 from rest_framework import status, generics
 from rest_framework.exceptions import ValidationError
 from rest_framework.response import Response
 
-from core.models import TaskActivity
+from core.models import TaskActivity, Task
 from core.serializers import CreateTaskActivitySerializer
 
 
@@ -11,7 +13,6 @@ class StartTaskActivityView(generics.CreateAPIView):
     queryset = TaskActivity.objects.all()
     serializer_class = CreateTaskActivitySerializer
 
-    @transaction.atomic
     def create(self, request, *args, **kwargs):
         user_ = request.user
         # Проверяем есть ли данная таска в работе у юзера
@@ -22,3 +23,22 @@ class StartTaskActivityView(generics.CreateAPIView):
         serializer.validated_data['user'] = user_
         serializer.save()
         return Response(status=status.HTTP_201_CREATED)
+
+
+class StopTaskActivityView(generics.UpdateAPIView):
+    queryset = Task.objects.all()
+    serializer_class = CreateTaskActivitySerializer
+    lookup_field = 'id'
+
+    def update(self, request, *args, **kwargs):
+        task = self.get_object()
+        try:
+            task_activity = TaskActivity.objects.get(user=request.user,
+                                                     task=task,
+                                                     finished_at__isnull=True
+                                                     )
+            task_activity.finished_at = datetime.datetime.now()
+            task_activity.save()
+        except ObjectDoesNotExist:
+            raise ValidationError(code=400, detail='Задача не взята в работу')
+        return Response(status=status.HTTP_200_OK)
