28561a9047f76dc69654c535e61bc311907937e0
Rafael Aslanyan <rafaaslan923@gmail.com>
2024-02-27 21:23:09 +0300

    Add djoser(temporary)

--- time_tracker/time_tracker/settings.py
+++ time_tracker/time_tracker/settings.py
@@ -35,6 +35,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework.authtoken',
+    'djoser',
 
     # наши собственные приложения
     'core',
@@ -121,3 +124,13 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'users.User'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',  # требование из тз
+    ],
+
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ]
+}
